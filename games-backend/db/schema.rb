20fc5dec8a699c03d92660ad1de31fbb7a1901a8
Brian Nicholls <nichol88@gmail.com>
2020-04-14 02:47:51 -0400

    add n-number to Trial

--- games-backend/db/schema.rb
+++ games-backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_11_073449) do
+ActiveRecord::Schema.define(version: 2020_04_14_064155) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2020_04_11_073449) do
     t.integer "max_turns"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "n_number"
   end
 
   create_table "turns", force: :cascade do |t|
