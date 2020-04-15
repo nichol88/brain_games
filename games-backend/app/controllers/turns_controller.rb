b7117357f82cd3f9c50d07d56bcb39371f67acf1
Brian Nicholls <nichol88@gmail.com>
2020-04-15 04:09:39 -0400

    add controller actions for turns

--- games-backend/app/controllers/turns_controller.rb
+++ games-backend/app/controllers/turns_controller.rb
@@ -9,12 +9,24 @@ class TurnsController < ApplicationController
   end
 
   def show
+    set_turn
+    render json: @turn
+  end
 
+  def update
+    set_turn
+    if @turn.update(turn_params)
+      render json: @turn.trial
+    end
   end
 
   private
 
+  def set_turn
+    @turn = Turn.find(params[:id])
+  end
+
   def turn_params
-    params.require(:turn).permit(:trial_id, :grid_position, :asset_id, :user_selected_audio, :user_selected_position)
+    params.require(:turn).permit(:trial_id, :grid_position, :asset_id, :user_selected_audio, :user_selected_position, :correct_audio_guess, :correct_position_guess)
   end
 end
